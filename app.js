92fd3c7036a256745596ceae14a3f88f3b1249c8
Gaston256 <nahurirabriangaston@gmail.com>
2022-07-02 16:42:10 +0300

    initial deploy hostel api

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const { Hostel } = require('./models/task')
+const { addhostel, registeruser, registeradmin, pendinghostel, edithostel, confirmedhostel, singlehostel, deletehostel } = require('./routes/tasks')
+const ConnectDB = require("./db/connect");
+// const cors = require("cors");
+const port = process.env.PORT || 5001;
+
+// app.use(cors); //i also put cors like to access cross origin sites
+app.use(express.json());
+
+//login apis
+app.use("/api/v6/",registeradmin)
+app.use("api/v6/",registeruser)
+
+
+
+// hostel apis
+app.get('/hostel/:id',function(req,res){
+    fetchid = req.params.id;
+    Hostel.find(({id:fetchid}), function(err, val){
+        res.send(val);
+    })
+} )
+app.use("/api/v6/", addhostel);
+app.use("api/v6/", confirmedhostel);
+app.use("/api/v6/",pendinghostel);
+app.use("api/v6/",edithostel);
+app.use("api/v6/", singlehostel);
+app.use("api/v6/", deletehostel);
+
+//database connectivity
+ConnectDB();
+
+app.listen(port, console.log(`server started on port: ${port}.......`));
